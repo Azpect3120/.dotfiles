f020c0581aa7fdb47d0749722b3172af42c2a5d8
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-02-04 14:05:26 -0700

    FEAT: Copilot mapped to Ctrl + y

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,4 +21,14 @@ return {
     "windwp/nvim-autopairs",
     opts = { map_cr = true }
   },
+  {
+    "github/copilot.vim",
+    config = function ()
+        vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+          expr = true,
+          replace_keycodes = false
+        })
+        vim.g.copilot_no_tab_map = true
+    end
+  }
 }
