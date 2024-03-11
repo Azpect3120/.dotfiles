3cdd2b9acdfc2169c9ebc8e1d6047323bef1ffef
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-03-10 18:11:42 -0700

    FEAT: Added PSQL completitions?

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -1,10 +1,10 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
+  "kristijanhusak/vim-dadbod-ui",
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "psql" }, lazy = true },
   },
-  cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
+  cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
   keys = {
     { "<leader>db", "<cmd>DBUIToggle<cr>", desc = "Toggle the DB UI" },
     { "<leader>dba", "<cmd>DBUIAddConnection<cr>", desc = "Create a new DB connection" },
