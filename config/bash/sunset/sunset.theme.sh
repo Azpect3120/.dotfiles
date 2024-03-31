96dbc5d639fc0a758d0dd0d0b8da46da5c38c0e4
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-03-31 00:09:40 -0700

    FEAT: New bash theme!

--- config/bash/sunset/sunset.theme.sh
+++ config/bash/sunset/sunset.theme.sh
@@ -1,14 +1,15 @@
 #! bash oh-my-bash.module
 
 function _omb_theme_PROMPT_COMMAND() {
-  local arrow="${_omb_prompt_bold_blue} ♠${_omb_prompt_reset_color}"
-  local base_directory="${_omb_prompt_bold_yellow}\w${_omb_prompt_reset_color}"
-  local GIT_THEME_PROMPT_PREFIX="${_omb_prompt_bold_purple}⟪ ${_omb_prompt_reset_color}"
-  local SCM_THEME_PROMPT_SUFFIX="${_omb_prompt_bold_purple} ⟫${_omb_prompt_reset_color}"
+  # local arrow="${_omb_prompt_bold_blue} ♠${_omb_prompt_reset_color}"
+  local arrow="${_omb_prompt_bold_green}  ${_omb_prompt_reset_color}"
+  local base_directory="${_omb_prompt_bold_green}\w${_omb_prompt_reset_color}"
+  local GIT_THEME_PROMPT_PREFIX="${_omb_prompt_bold_purple}  ${_omb_prompt_bold_yellow}"
+  local SCM_THEME_PROMPT_SUFFIX="${_omb_prompt_bold_purple} ${_omb_prompt_reset_color}"
   local SCM_THEME_PROMPT_CLEAN="${_omb_prompt_bold_green} ✔${_omb_prompt_reset_color}"
   local SCM_THEME_PROMPT_DIRTY="${_omb_prompt_bold_red} ✘${_omb_prompt_reset_color}"
 
-  PS1="${arrow}  ${base_directory} "
+  PS1="${arrow} ${base_directory} "
 
   local python_venv
   _omb_prompt_get_python_venv
