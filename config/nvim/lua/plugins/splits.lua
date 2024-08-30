bcdb8b54de8d09863c14777b5bc433fea913c954
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-08-29 17:30:41 -0700

    FIX: Better split controls in neovim to match I3

--- config/nvim/lua/plugins/splits.lua
+++ config/nvim/lua/plugins/splits.lua
@@ -4,10 +4,10 @@ return {
 
   },
   config = function ()
-    vim.keymap.set("n", "<A-H>", require("smart-splits").resize_left)
-    vim.keymap.set("n", "<A-J>", require("smart-splits").resize_down)
-    vim.keymap.set("n", "<A-K>", require("smart-splits").resize_up)
-    vim.keymap.set("n", "<A-L>", require("smart-splits").resize_right)
+    vim.keymap.set("n", "<C-A-h>", require("smart-splits").resize_left)
+    vim.keymap.set("n", "<C-A-j>", require("smart-splits").resize_down)
+    vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
+    vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
 
     -- moving between splits
     vim.keymap.set("n", "<A-h>", require("smart-splits").move_cursor_left)
@@ -16,10 +16,10 @@ return {
     vim.keymap.set("n", "<A-l>", require("smart-splits").move_cursor_right)
 
     -- swapping buffers between windows
-    vim.keymap.set("n", "<C-A-h>", require("smart-splits").swap_buf_left)
-    vim.keymap.set("n", "<C-A-j>", require("smart-splits").swap_buf_down)
-    vim.keymap.set("n", "<C-A-q>", require("smart-splits").swap_buf_up)
-    vim.keymap.set("n", "<C-A-l>", require("smart-splits").swap_buf_right)
+    vim.keymap.set("n", "<A-H>", require("smart-splits").swap_buf_left)
+    vim.keymap.set("n", "<A-J>", require("smart-splits").swap_buf_down)
+    vim.keymap.set("n", "<A-K>", require("smart-splits").swap_buf_up)
+    vim.keymap.set("n", "<A-L>", require("smart-splits").swap_buf_right)
 
     -- creating and killing splits
     vim.keymap.set("n", "<A-'>", "<cmd>vsplit<cr><C-w>l")
--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -9,6 +9,7 @@ return {
     -- refer to the configuration section below
   },
   keys = {
-    { "<leader>tt", "<cmd>Trouble diagnostics<cr>", desc = "Toggle the error log" }
+    { "<leader>tt", "<cmd>Trouble diagnostics toggle win.position=right<cr>", desc = "Toggle the error log" },
+    { "<leader>ts", "<cmd>Trouble symbols toggle<cr>", desc = "Toggle the symbols from trouble" }
   }
 }
